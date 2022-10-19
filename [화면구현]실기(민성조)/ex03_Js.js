3096124818c3a72ec52ca4530d1606c79a6e3e75
Min-SungJo <123or@naver.com>
2022-10-19 16:33:30 +0900

    front

---
+++ [화면구현]실기(민성조)/ex03_Js.js
@@ -0,0 +1,13 @@
+function plus(){
+    document.getElementById("num").innerText++;
+}
+
+function minus(){
+    document.getElementById("num").innerText>0
+    &&document.getElementById("num").innerText--;
+}
+
+function reset(){
+    document.getElementById("num").innerText=0
+    
+}
